a840fdc21c5ffff33b409afaa31380e4683cfed2
Da-Youn <dayoun202@gmail.com>
2024-10-11 18:07:19 +0900

    Feat: 닉네임 입력 페이지 인풋 입력 기능 적용 - 유효성 검사 설정 (글자수, 공백, 특수문자 제한)

--- src/app/(auth)/login/nickname/page.tsx
+++ src/app/(auth)/login/nickname/page.tsx
@@ -1,9 +1,27 @@
+'use client';
+
 import LargeButton from '<prefix>/components/common/button/largeButton';
 import Input from '<prefix>/components/common/input';
+import useInput from '<prefix>/hooks/useInput';
+import {
+  maxLenRegExp,
+  specialCharRegExp,
+  whiteSpaceRegExp,
+} from '<prefix>/shared/utils/regex';
+import Link from 'next/link';
 export default function NickNamePage() {
+  const validator = (value: string) => {
+    return (
+      maxLenRegExp(8).test(value) &&
+      !whiteSpaceRegExp.test(value) &&
+      !specialCharRegExp.test(value)
+    );
+  };
+  const [inputValue, handleInputChange] = useInput<string>('', validator);
+
   return (
-    <>
-      <div className='mb-32 space-y-6 pl-16 pt-145'>
+    <div className='px-16'>
+      <div className='mb-32 space-y-6 pt-145'>
         <h2 className='text-head-01 text-neutral-900'>
           맘보에 오신 것을 환영해요! <br /> 사용하실 닉네임을 알려주세요.
         </h2>
@@ -11,15 +29,22 @@ export default function NickNamePage() {
           닉네임은 언제든지 변경할 수 있어요.
         </p>
       </div>
-      <Input placeholder='닉네임을 입력해주세요.' />
-      <LargeButton
-        variant='fill'
-        buttonColor='primary'
-        className='absolute bottom-40'
-        disabled={true}
-      >
-        다음
-      </LargeButton>
-    </>
+      <Input
+        value={inputValue}
+        maxLength={8}
+        placeholder='닉네임을 입력해주세요.'
+        onChange={handleInputChange}
+      />
+      <Link href='/login/usertype'>
+        <LargeButton
+          variant='fill'
+          buttonColor='primary'
+          className='absolute bottom-40'
+          disabled={!!inputValue === false}
+        >
+          다음
+        </LargeButton>
+      </Link>
+    </div>
   );
 }
