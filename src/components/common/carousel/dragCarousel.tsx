91d91be5bb60e30711e05c84bd2b93c567175c26
Da-Youn <dayoun202@gmail.com>
2024-10-14 20:59:41 +0900

    Design: 드래그 캐러셀, 자동넘김 캐러셀 UI 컴포넌트 구현

---
+++ src/components/common/carousel/dragCarousel.tsx
@@ -0,0 +1,47 @@
+import useCarousel from '<prefix>/hooks/useCarousel';
+import useCarouselSize from '<prefix>/hooks/useCarouselSize';
+
+type DragCarouselProps<T> = {
+  items: T[];
+  gap?: number;
+  slideWidth?: number;
+  renderItem: (item: T, index: number) => React.ReactNode;
+};
+
+export default function DragCarousel<T>({
+  items,
+  gap = 8,
+  slideWidth = 328,
+  renderItem,
+}: DragCarouselProps<T>) {
+  const { ref, width } = useCarouselSize();
+
+  const { currentIndex, transX, onTouchStart, onMouseDown } = useCarousel({
+    maxIndex: items.length - 1,
+    resetFirstIndex: false,
+    slideWidth,
+  });
+
+  const maxTranslateX =
+    slideWidth * items.length -
+    width +
+    (currentIndex < items.length - 1 ? 0 : gap * (items.length - 1));
+
+  return (
+    <div ref={ref} className='relative w-full overflow-hidden'>
+      <div
+        className={`flex gap-${gap ? gap : 0}`}
+        style={{
+          transform: `translateX(${-Math.min(currentIndex * (slideWidth + gap), maxTranslateX) + transX}px)`,
+          transition: `transform ${transX ? 0 : 300}ms ease-in-out 0s`,
+        }}
+        onTouchStart={onTouchStart}
+        onMouseDown={onMouseDown}
+      >
+        {items.map((item, index) => (
+          <> {renderItem(item, index)}</>
+        ))}
+      </div>
+    </div>
+  );
+}
