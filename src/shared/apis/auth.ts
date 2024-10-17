38e866b49cf6da8720a6af34443e8a113d33cb21
junny97 <pcohad12@gmail.com>
2024-10-17 10:32:47 +0900

    Fix: join api endpoint 수정

--- src/shared/apis/auth.ts
+++ src/shared/apis/auth.ts
@@ -2,6 +2,6 @@ import { IJoinReq } from '../types/auth';
 import axiosInstance from './axiosInstance';
 
 export const createUserInfo = async (data: IJoinReq) => {
-  const response = await axiosInstance.post('/user/join', data);
+  const response = await axiosInstance.post('/user/join/', data);
   return response.data;
 };
