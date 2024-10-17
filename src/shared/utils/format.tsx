b47103f69eb8c99e0e3e2e6a859e52a15aaa05dd
Da-Youn <dayoun202@gmail.com>
2024-10-17 23:46:04 +0900

    Feat: 툴팁 컴포넌트 구현 - 12개 방향의 direction을 props로 받아 위치 이동 가능 - 버튼 컴포넌트 / 모달 컴포넌트 분리 고민..

---
+++ src/shared/utils/format.tsx
@@ -0,0 +1,14 @@
+export function formatNumberWithCommas(number: number): string {
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+export function formatLines(text: string) {
+  const lines = text.split('\n');
+  console.log(lines);
+  return lines.map((line, index) => (
+    <>
+      {line}
+      {index < lines.length - 1 && <br />}
+    </>
+  ));
+}
