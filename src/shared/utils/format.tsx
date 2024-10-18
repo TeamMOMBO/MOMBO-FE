5d0d425ed20faa958e24dcf6de86f9c9456ae84d
Da-Youn <dayoun202@gmail.com>
2024-10-18 15:47:52 +0900

    Fix: formatLines 반환 시 key props 추가

--- src/shared/utils/format.tsx
+++ src/shared/utils/format.tsx
@@ -1,3 +1,5 @@
+import { Fragment } from 'react';
+
 export function formatNumberWithCommas(number: number): string {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 }
@@ -6,9 +8,9 @@ export function formatLines(text: string) {
   const lines = text.split('\n');
   console.log(lines);
   return lines.map((line, index) => (
-    <>
+    <Fragment key={index}>
       {line}
       {index < lines.length - 1 && <br />}
-    </>
+    </Fragment>
   ));
 }
