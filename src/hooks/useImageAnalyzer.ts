eb3bce9f2bdc7d8d3bf3c6f2421c705e4cb3b8d4
junny97 <pcohad12@gmail.com>
2024-10-21 14:26:12 +0900

    Feat: 성분 분석 결과 스토어 설정

--- src/hooks/useImageAnalyzer.ts
+++ src/hooks/useImageAnalyzer.ts
@@ -3,10 +3,14 @@ import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 import { createIngredientAnalysis } from '<prefix>/shared/apis/ingredient';
 import { IAnalysisResult } from '<prefix>/shared/types/ingredient';
+import { useIngredientAnalysisStore } from '<prefix>/state/store/IngredientAnalysisStore';
 
 export default function useImageAnalyzer() {
   const router = useRouter();
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const setAnalysisResult = useIngredientAnalysisStore(
+    (state) => state.setAnalysisResult,
+  );
   //   const [analysisResult, setAnalysisResult] = useState<IAnalysisResult | null>(
   //     null,
   //   );
@@ -20,6 +24,7 @@ export default function useImageAnalyzer() {
     onSuccess: (data) => {
       console.log(data);
       router.push('/ingredient/result');
+      setAnalysisResult(data);
       //   setAnalysisResult(data);
       //성공 응답 data 전역 상태관리?
     },
