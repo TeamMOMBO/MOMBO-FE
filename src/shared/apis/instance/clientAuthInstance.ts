f63b657d528be9f0dd59c22ad801f107afc40e35
junny97 <pcohad12@gmail.com>
2024-12-05 14:34:55 +0900

    Refactor: respone, request 인터셉터 로직 파일 분리

--- src/shared/apis/instance/clientAuthInstance.ts
+++ src/shared/apis/instance/clientAuthInstance.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
-import { getCookie } from 'cookies-next';
+import {
+  requestErrorInterceptor,
+  requestInterceptor,
+  responseErrorInterceptor,
+  responseInterceptor,
+} from '../interceptor/authInterceptor';
 
 const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
@@ -8,36 +13,13 @@ export const clientAuthInstance = axios.create({
 });
 
 clientAuthInstance.interceptors.request.use(
-  (config) => {
-    const accessToken = getCookie('accessToken');
-    if (accessToken) {
-      config.headers.Authorization = `Bearer ${accessToken}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
+  requestInterceptor,
+  requestErrorInterceptor,
 );
 
-//응답 에러
 clientAuthInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-
-      try {
-        // 미들웨어에서 토큰 갱신 후 재요청
-        const newAccessToken = getCookie('accessToken');
-        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
-        return await clientAuthInstance(originalRequest);
-      } catch (refreshError) {
-        window.location.href = '/login';
-        return Promise.reject(refreshError);
-      }
-    }
-
-    return Promise.reject(error);
-  },
+  responseInterceptor,
+  responseErrorInterceptor,
 );
+
+export default clientAuthInstance;
