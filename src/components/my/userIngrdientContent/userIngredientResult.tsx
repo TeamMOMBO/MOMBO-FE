e8a0265abf21b132b8973f7bda6af4c47266a899
Da-Youn <dayoun202@gmail.com>
2024-12-17 20:59:40 +0900

    Feat: 계정 정보 수정 이동 Link태그 추가

--- src/components/my/userIngrdientContent/userIngredientResult.tsx
+++ src/components/my/userIngrdientContent/userIngredientResult.tsx
@@ -3,6 +3,7 @@ import RightArrowIcon from '/public/svgs/arrow/icon-right.svg';
 import UseringredientResultList from './useringredientResultList';
 import { useUserProfileQuery } from '<prefix>/state/queries/user';
 import ProgressBar from '<prefix>/components/main/progressBar';
+import Link from 'next/link';
 
 const totalWeek = 40;
 
@@ -16,9 +17,11 @@ export default function UserIngredientResult() {
           <span className='text-head-01 text-neutral-900'>
             {userProfile?.profile.nickname} 님
           </span>
-          <RightArrowIcon
-            className={`h-24 w-24 cursor-pointer items-center justify-center stroke-neutral-600`}
-          />
+          <Link href='/my/edit'>
+            <RightArrowIcon
+              className={`h-24 w-24 cursor-pointer items-center justify-center stroke-neutral-600`}
+            />
+          </Link>
         </div>
         <span className='mb-20 text-body-06 text-neutral-600'>
           임신 {userProfile?.profile.pregnancyWeek ?? 0}주 차
