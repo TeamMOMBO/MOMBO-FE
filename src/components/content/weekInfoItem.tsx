920f53b8ab8c5d6e7ae817ab3858dd96a56b6844
Da-Youn <dayoun202@gmail.com>
2024-12-30 21:05:13 +0900

    Design: 주차별 정보 리스트 UI 구현

--- src/components/content/weekInfoItem.tsx
+++ src/components/content/weekInfoItem.tsx
@@ -1,5 +1,27 @@
-type Props = {};
+import Image from 'next/image';
+import React from 'react';
+import EyeIcon from '/public/svgs/light/icon-eye.svg';
+import { formatNumberWithCommas } from '<prefix>/shared/utils/format';
+import Link from 'next/link';
+import { WeekInfoResponse } from '<prefix>/shared/types/content';
 
-export default function WeekInfoItem({}: Props) {
-  return <div></div>;
+type FaqItemProps = {
+  weekInfoItem: WeekInfoResponse;
+};
+
+export default function WeekInfoItem({ weekInfoItem }: FaqItemProps) {
+  const { id, week, summary, maternity, fetus } = weekInfoItem;
+  return (
+    <Link className='flex h-81 w-full gap-12' href={`/weekInfo/${id}`}>
+      <div className='flex flex-col gap-4'>
+        <p className='text-overflow shrink-0 text-body-04 text-neutral-900'>
+          <span className='text-primary'>{week}주차 </span>
+          {maternity}
+        </p>
+        <span className='text-overflow-1 text-body-10 text-neutral-600'>
+          {summary}
+        </span>
+      </div>
+    </Link>
+  );
 }
