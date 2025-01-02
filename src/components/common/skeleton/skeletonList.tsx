fa6bcb22bf9e9c1079d41d3f4332e631c47d72b4
junny97 <pcohad12@gmail.com>
2025-01-02 13:07:48 +0900

    Design: 홈 스켈레톤UI 수정

--- src/components/common/skeleton/skeletonList.tsx
+++ src/components/common/skeleton/skeletonList.tsx
@@ -7,12 +7,12 @@ interface SkeletonListProps {
 export default function SkeletonList({ count }: SkeletonListProps) {
   return (
     <>
-      {/* TabMenu 영역과 동일한 여백 유지 */}
+      {/* TabMenu 영역*/}
       <div className='pb-20 pl-16 pt-13'>
-        <div className='h-[40px] w-[200px] bg-neutral-200 rounded-6 animate-pulse' />
+        <div className='h-[40px] w-[200px] animate-pulse rounded-6 bg-neutral-200' />
       </div>
 
-      {/* VirtualList 영역과 동일한 구조 */}
+      {/* VirtualList 영역*/}
       <div className='px-16'>
         <div
           style={{
@@ -28,13 +28,10 @@ export default function SkeletonList({ count }: SkeletonListProps) {
               >
                 <div className='flex items-center justify-between'>
                   <div className='flex items-center gap-6'>
-                    {/* 성분 이름 */}
-                    <div className='w-[200px] h-[24px] bg-neutral-200 rounded-6 animate-pulse' />
-                    {/* 등급 표시 */}
-                    <div className='w-[48px] h-[20px] bg-neutral-200 rounded-6 animate-pulse' />
+                    <div className='h-[24px] w-[200px] animate-pulse rounded-6 bg-neutral-200' />
+                    <div className='h-[20px] w-[48px] animate-pulse rounded-6 bg-neutral-200' />
                   </div>
-                  {/* 화살표 아이콘 */}
-                  <div className='w-24 h-24 bg-neutral-200 rounded-full animate-pulse' />
+                  <div className='h-24 w-24 animate-pulse rounded-full bg-neutral-200' />
                 </div>
               </div>
             ))}
