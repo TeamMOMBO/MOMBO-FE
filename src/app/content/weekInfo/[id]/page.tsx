15e4e9af3fcecedbef0d6db24b106401fed93356
Da-Youn <dayoun202@gmail.com>
2025-02-22 22:48:24 +0900

    Design: 주차별 정보 상세 페이지 구현

--- src/app/content/weekInfo/[id]/page.tsx
+++ src/app/content/weekInfo/[id]/page.tsx
@@ -1,3 +1,8 @@
+import FAQDetail from '<prefix>/components/content/faq/faqDetail';
+import WeekInfoDetail from '<prefix>/components/content/weekInfo/weekInfoDetail';
+import { getContentServer } from '<prefix>/shared/apis/serverApi/content/contentDetail.server.api';
+import { WeekInfoResponse } from '<prefix>/shared/types/content';
+
 type Props = {
   params: {
     id: string;
@@ -6,6 +11,16 @@ type Props = {
 
 export default async function FAQPage({ params }: Props) {
   const { id } = params;
+  const data = await getContentServer({
+    category: 'info',
+    postNo: parseInt(id),
+  });
 
-  return <div className='flex h-full flex-col'></div>;
+  const weekInfoData = data?.result as WeekInfoResponse;
+  return (
+    <>
+      <h1 className='sr-only'>콘텐츠 상세</h1>
+      <WeekInfoDetail weekInfoData={weekInfoData} />
+    </>
+  );
 }
