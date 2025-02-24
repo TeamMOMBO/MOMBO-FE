efbe5b8e5df15157e125f535702993f156394fc8
junny97 <pcohad12@gmail.com>
2025-02-24 13:02:32 +0900

    Feat: 성분 사전 오름차순, 내림차순 정렬 기능추가

--- src/state/queries/ingredient.ts
+++ src/state/queries/ingredient.ts
@@ -3,11 +3,14 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 import { IngredientDictionaryResponse } from '<prefix>/shared/types/ingredient';
 import { THREE_MIN_STALE_TIME } from '<prefix>/shared/constants/queryOptions';
 
-export const useIngredientDictionaryQuery = (sort: 'name' | 'level') => {
+export const useIngredientDictionaryQuery = (
+  sort: 'name' | 'level',
+  order: 'asc' | 'desc',
+) => {
   return useInfiniteQuery<IngredientDictionaryResponse>({
-    queryKey: ['ingredient-dictionary', sort],
+    queryKey: ['ingredient-dictionary', sort, order],
     queryFn: ({ pageParam = 1 }) =>
-      getIngredientDictionary({ page: pageParam as number, sort }),
+      getIngredientDictionary({ page: pageParam as number, sort, order }),
     getNextPageParam: (lastPage) =>
       lastPage.next ? lastPage.results.page + 1 : undefined,
     initialPageParam: 1,
